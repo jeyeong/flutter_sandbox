c5fbac5a503493e3abc91b5418b434c7daeff4a5
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-10-18 16:35:36 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget generateContainer() {
+    return Container(
+      margin: EdgeInsets.all(20),
+      width: 50,
+      height: 50,
+      color: Colors.blue,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter layout demo'),
+          ),
+          body: Container()),
+    );
+  }
+}
